bdf846f3cef3cf86b54893e2db73bfb84916337d
kkasperek <kt.kasperek@gmail.com>
2020-02-27 21:16:54 -0600

    modify endpoint

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-.get('/times', (req, res) => res.send(showTimes()))
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showTimes = () => {
