54ed5e27d59ecda43acee7595d1cd8823058e480
kkasperek <kt.kasperek@gmail.com>
2020-02-27 21:11:49 -0600

    define heroku config vars

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+.get('/times', (req, res) => res.send(showTimes()))
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+showTimes = () => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result;
+}
